4b054f792586a71110f9b68add1a9b1cb9791c41
Kauã Marques <kauamarkkes@gmail.com>
2026-05-13 20:35:59 -0300

    recuperação do projeto

--- backend/catalogo/urls.py
+++ backend/catalogo/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('produto/<int:id>/', views.detalhe_produto, name='detalhe_produto'),
     path('painel-admin/', views.dashboard_admin, name='dashboard_admin'),
-    path('carrinho/', views.ver_carrinho, name='ver_carrinho'),path('add/<int:produto_id>/',views.adicionar_ao_carrinho, name='add_carrinho'),
 ]
--- backend/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from backend.views import index, login, cadastro
-
-urlpatterns = [
-    path('', index),
-    path('login/', login, name= 'login'),
-    path('cadastro/', cadastro, name= 'cadastro'),
-]
